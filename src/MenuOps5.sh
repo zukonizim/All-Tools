44c8daf8fdb3705139f56aa7e60ee2420b6a1b11
zuko <zukonizim@gmail.com>
2025-11-25 21:43:42 +0300

    Update MenuOps5.sh

--- src/MenuOps5.sh
+++ src/MenuOps5.sh
@@ -9,10 +9,10 @@ echo '
    ├ Tools for special operations 
    ├ blackmail utilities And human use
   [SKIP]┐
-      ┌[03] >> Sniffing and Spoofing:  -  Tools for forgery of data and databases
-      │[04] >> Web Attack Tools:  -  Tools for hacking websites and servers
-  [-]─┘[05] >> Cam Hacking Tools:  -  Tools for hacking cams and front camera
-       [06] >> Remote Trojan RAT:  -  Utilities for creating RAT virus
+      ┌[03] >> Sniffing and Spoofing:  -  Veri ve veritabanlarının sahteciliği için araçlar
+      │[04] >> Web Attack Tools:  -  Web sitelerini ve sunucuları hacklemek için araçlar
+  [-]─┘[05] >> Cam Hacking Tools:  -  Kameraları ve ön kamerayı hacklemek için araçlar RAT
+       [06] >> Remote Trojan RAT:  -  RAT virüsü oluşturmak için yardımcı programlar
       ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣        
    ╔[NOTICE] Useful Tools 
   [07] >> SQL injection Tool:
@@ -23,15 +23,15 @@ echo '
    ├ And Bruteforce social networks
   [>]┐
    ┌[SKIP]
-  [09] >> Bombing & Spamming Tools:  -  Utilities for sms bombing And Sending Anonymous SMS
-  [10] >> Vulnerability Analysis:  -  Systems for vulnerability analysis 
-  [11] >> DarkSearch Tools:      -  Tools for search in DarkNet & DeepWeb
-  [12] >> Phishing And IpHack:  -  Tools for phishing and fake websites
-  [13] >> Passworld Attack:    -  Utilities for cracking passwords
-  [14] >> Wordlist Generator:  -  Tools for Genering Worldlist
-  [15] >> XSS Attack Tools:  -  Utilities for Attacking XSS
-  [16] >> Discord leaks:   - Discord mass users information
-  [17] >> Telegram Info: - Mass Telegram users information
+  [09] >> Bombing & Spamming Tools:  -  SMS Bombalama ve Anonim SMS Gönderme Yardımcı Programları
+  [10] >> Vulnerability Analysis:  -  Güvenlik açığı analizine yönelik sistemler
+  [11] >> DarkSearch Tools:      -  DarkNet ve DeepWebde arama araçları
+  [12] >> Phishing And IpHack:  -  Kimlik avı ve sahte web siteleri için araçlar
+  [13] >> Passworld Attack:    -  Şifreleri kırmak için yardımcı programlar
+  [14] >> Wordlist Generator:  -  WorldList Oluşturma Araçları
+  [15] >> XSS Attack Tools:  -  XSS Saldırılarına Yönelik Yardımcı Programlar
+  [16] >> Discord leaks:   - Toplu kullanıcı bilgilerinin uyumsuzluğu
+  [17] >> Telegram Info: - Telegram kullanıcı bilgileri
 
       ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣    ⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣⁣  ⁣
   [18] >> Other tools: 
@@ -39,10 +39,10 @@ echo '
   [19] >> Terminal Panel: 
    ├ Termux control and special features
   [SKIP]┐
-      ┌[20] >> System Settings:  -  AllHackingTools control and special features
-      │[21] >> System License:  -  View the license in AllHackingTools
-  [-]─┘[22] >> Update System:  -  Update system and all tools & menu 
-       [23] >> About System:  -  View system information and developer
-       [24] >> Exit System:  -  Log out of the AllHackingTools system 
-       [25] >> Log Out Terminal  -  Log out in AllHackingTools And Terminal'|lolcat -p 4.6
+      ┌[20] >> System Settings:  -  All-Tools kontrolü ve özel özellikleri
+      │[21] >> System License:  -  All-Toolsdaki lisansı görüntüleyin
+  [-]─┘[22] >> Update System:  -  Sistemi ve tüm araçları ve menüyü güncelle
+       [23] >> About System:  -  Sistem bilgilerini ve geliştiriciyi görüntüleyin
+       [24] >> Exit System:  -  All-Tools sisteminden çıkış yapın
+       [25] >> Log Out Terminal  -  All-Tools And Terminalden çıkış yapın ' |lolcat -p 4.6
 
