2503e83756ca5584ee2416ebdd194b5dc2fb5005
zuko <zukonizim@gmail.com>
2025-11-26 23:00:29 +0300

    Update Modules.sh

--- Files/Modules.sh
+++ Files/Modules.sh
@@ -126,7 +126,7 @@ echo '
 ██║░░██║██║░░██║██║╚████║██╔══╝░░
 ██████╔╝╚█████╔╝██║░╚███║███████╗
 ╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝
-Developer : mishakorzhik
+Developer : zuko
 created on: 23 05 2021
 code      : python, bash, php
 '|lolcat -p 1.0
