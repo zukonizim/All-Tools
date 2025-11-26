5c83f49128017f29c366531913253e047f5f89d6
zuko <zukonizim@gmail.com>
2025-11-26 20:37:39 +0300

    Update CreateTermuxBannerRusto.sh

--- src/CreateTermuxBannerRusto.sh
+++ src/CreateTermuxBannerRusto.sh
@@ -26,7 +26,7 @@ echo -e "\e[1m\e[33m\nEnter Your Cowsay Name\e[32m :\n\n "
 read eycn
 echo  "clear && cowsay -f eyes "$eycn" | lolcat -p 1.0" > C0w54y.txt
 echo "toilet -f rusto ' $eybn' -F gay | lolcat -p 1.0" > 84nn3r.txt
-echo "cd && bash AllHackingTools/Tool/UserInfo.sh" > U3eR1nf0.txt
+echo "cd && bash All-Tools/Tool/UserInfo.sh" > U3eR1nf0.txt
 echo
 cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/zshrc
 ls
