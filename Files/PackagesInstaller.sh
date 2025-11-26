462279ee9b0bd5a372f05e8d24108c02e8250afb
zuko <zukonizim@gmail.com>
2025-11-26 23:01:20 +0300

    Update PackagesInstaller.sh

--- Files/PackagesInstaller.sh
+++ Files/PackagesInstaller.sh
@@ -14,7 +14,7 @@ echo -e "$yellow |___|___|  /____  > |__| (____  /____/____/\___  >__|    "
 echo -e "$yellow          \/     \/            \/               \/        "
 echo ""
 echo -e "$orange [>] $yellow Tool Name: All-Tools "
-echo -e "$orange [>] $yellow Developer: Misha Korzhik " 
+echo -e "$orange [>] $yellow Developer: zuko " 
 
 which git > /dev/null 2>&1
 if [ "$?" -eq "0" ]; then
