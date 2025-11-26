1fb8524a5d8c1be23e10a40b9ed5c303e0aded63
zuko <zukonizim@gmail.com>
2025-11-26 23:42:08 +0300

    Update AnimationLoad1.sh

--- .settings/debug/AnimationLoad1.sh
+++ .settings/debug/AnimationLoad1.sh
@@ -8,7 +8,7 @@ printf "\e[1;92m"
 
 cd
 cd
-cd AllHackingTools 
+cd All-Tools 
 cd src
 python3 ProgressBar.py
 python3 Packages.py
@@ -16,5 +16,5 @@ sleep 2
 clear
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 python2 MainMenu.py
