f1c7ac03c820d381100421e2c36761544db319ff
zuko <zukonizim@gmail.com>
2025-11-26 21:51:24 +0300

    Update AndroidFiles.sh

--- Files/AndroidFiles.sh
+++ Files/AndroidFiles.sh
@@ -1,18 +1,18 @@
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/knassar702/pyshell
 cd Pyshell
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/kinghacker0/WishFish
 cd 
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/JasonJerry/lockphish
 cd lockphish
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/HatBashBR/HatCloud
 cd
 cd
-cd AllHackingTools
+cd All-Tools
