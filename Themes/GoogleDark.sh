70f7c97bb890d777425cacd060f66c6fb47a1c84
zuko <zukonizim@gmail.com>
2025-11-26 21:21:40 +0300

    Update GoogleDark.sh

--- Themes/GoogleDark.sh
+++ Themes/GoogleDark.sh
@@ -2,5 +2,5 @@ cd
 cd
 rm -rf ~/.termux/colors.properties
 rm -rf /data/data/com.termux/files/usr/etc/motd
-cd ~/AllHackingTools/Themes/.object ; cp -r .colors.properties7 ~/.termux/colors.properties
+cd ~/All-Tools/Themes/.object ; cp -r .colors.properties7 ~/.termux/colors.properties
 am broadcast --user 0 -a com.termux.app.reload_style com.termux > /dev/null
