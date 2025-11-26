615dd56029bac8b78239a3a44ff6de18c505bc08
zuko <zukonizim@gmail.com>
2025-11-26 21:58:49 +0300

    Update DarkDump.py

--- Files/DarkDump.py
+++ Files/DarkDump.py
@@ -5,10 +5,10 @@ import random
 import sys
 os.system("clear")
 os.system("cd")
-os.system("cd AllHackingTools")
+os.system("cd All-Tools")
 os.system("bash Logo.sh")
 os.system("cd darkdump")
 site=int(raw_input("\033[1;33;40mOnion Search:"))
 os.system("python3 darkdump.py '+site")
 os.system("cd")
-os.system("cd AllHackingTools")
+os.system("cd All-Tools")
