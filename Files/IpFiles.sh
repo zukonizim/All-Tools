197135554c96bfc6fb89199a044bb6a77895fb26
zuko <zukonizim@gmail.com>
2025-11-26 22:09:36 +0300

    Update IpFiles.sh

--- Files/IpFiles.sh
+++ Files/IpFiles.sh
@@ -1,49 +1,49 @@
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/UndeadSec/EvilURL.git
 cd EvilURL
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/ciku370/OSIF
 cd OSIF
 pip2 install -r requirements.txt
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/Cvar1984/Easymap
 cd Easymap
 bash install.sh
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/Gameye98/AstraNmap
 cd
 cd 
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/evait-security/weeman
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/exlinee/MaxSubdoFinder
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/jofpin/trape.git
 cd trape
 python2 -m pip install -r requirements.txt
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/Tuhinshubhra/RED_HAWK
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/Lulz3xploit/LittleBrother
 cd LittleBrother
 python3 -m pip install -r requirements.txt
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/thewhiteh4t/seeker.git
 cd seeker
 chmod +x install.sh
@@ -51,43 +51,43 @@ chmod +x install.sh
 rm -rf seeker.py 
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 cd Castom 
 chmod +x seeker.py
 cp seeker.py /data/data/com.termux/files/home/
 cd
 cd
-mv seeker.py /data/data/com.termux/files/home/AllHackingTools/seeker/ 
+mv seeker.py /data/data/com.termux/files/home/All-Tools/seeker/ 
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/s0md3v/ReconDog
 cd ReconDog
 chmod +x dog
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/bibortone/D-Tech
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 pip install iphack
 pip3 install iphack
 cd 
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/sullo/nikto
 cd 
 cd
-cd AllHackingTools
+cd All-Tools
 wget https://raw.githubusercontent.com/crunchsec/ismtp/master/ismtp.py
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 git clone https://github.com/mishakorzik/MailFinder
 cd
 cd
-cd AllHackingTools
+cd All-Tools
 pip3 install whois
 pip3 install flask_socketio
 pip3 install flask_cors
