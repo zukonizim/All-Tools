cafc2c4b7232cf42c4b7d0cfe09fc8a70aab56eb
zuko <zukonizim@gmail.com>
2025-11-26 22:00:26 +0300

    Update DarkSearchMenu.py

--- Files/DarkSearchMenu.py
+++ Files/DarkSearchMenu.py
@@ -5,7 +5,7 @@ import random
 import sys
 
 os.system("clear")
-os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
+os.system("cd && cd All-Tools && clear && bash Logo.sh")
 
 print("  \033[1;34m[ 01 ] >> \033[1;36;40mDarkDump - Search The Deep Web")
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mExit System")
@@ -15,18 +15,18 @@ op=int(raw_input("DarkSearch: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && python2 Files/DarkDump.py && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd All-Tools && python2 Files/DarkDump.py && cd && cd All-Tools && python2 MainMenu.py")
 elif(op==2):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting utility...")
  sys.exit()
 elif(op==3):
  os.system("cd")
- os.system("cd AllHackingTools")
+ os.system("cd All-Tools")
  os.system("python2 MainMenu.py")
 else:
  print("\033[1;31;40mInvalid input. Reloading Tools") 
  time.sleep(1.6)
  os.system("cd")
- os.system("cd AllHackingTool")
+ os.system("cd All-Tool")
  os.system("python2 Files/DarkSearchMenu.py")
